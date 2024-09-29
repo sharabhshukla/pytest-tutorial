2cb0add43c0ab51cc44727d5c07509b487d26bd9
Sharabh Shukla <sharabh2019@gmail.com>
2024-09-29 21:52:39 +0530

    Add Location and Institute fixtures, update EducationalInstitute
    Include fixtures for Location and EducationalInstitute in tests/conftest.py to support better testing coverage. Additionally, enhance the EducationalInstitute model by adding Location, a timetable attribute, and methods for adjusting student and teacher counts in src/school.py.
    
    Signed-off-by: Sharabh Shukla <sharabh2019@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,8 @@
 import pytest
+
+from src.school import Location, EducationalInstitute
 from src.shapes import Rectangle
+
 
 @pytest.fixture
 def my_int_rectangle():
@@ -8,3 +11,18 @@ def my_int_rectangle():
 @pytest.fixture
 def my_float_rectangle():
     return Rectangle(15.5, 5.5)
+
+
+@pytest.fixture
+def delhi():
+    return Location(lat=1.5, long=5.5)
+
+
+@pytest.fixture
+def mumbai():
+    return Location(lat=10.50, long=3.25)
+
+
+@pytest.fixture
+def delhi_institute(delhi):
+    return EducationalInstitute(location=delhi, total_students=300, total_teachers=50, timetable=True)
