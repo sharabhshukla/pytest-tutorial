d914612998721d60baa91ac8e6aeff544db32e49
Sharabh Shukla <sharabh2019@gmail.com>
2024-09-29 16:24:18 +0530

    Add get_users function and corresponding tests
    Added a new function, get_users, to fetch user data from an external API using the `requests` library. Also, introduced unit tests to mock the API responses and verify the functionality of the get_users function, ensuring proper integration and reliability.
    
    Signed-off-by: Sharabh Shukla <sharabh2019@gmail.com>

--- src/service.py
+++ src/service.py
@@ -1,4 +1,4 @@
-
+import requests
 
 database = {
     1: "Alice",
@@ -8,3 +8,11 @@ database = {
 
 def get_user_from_db(user_id):
     return database.get(user_id)
+
+
+def get_users():
+    response = requests.get("https://jsonplaceholder.typicode.com/users")
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise requests.HTTPError
