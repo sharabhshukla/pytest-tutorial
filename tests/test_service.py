914cffe97892ef14ddc6e9b6d82631712e38e3ac
Sharabh Shukla <sharabh2019@gmail.com>
2024-09-29 16:24:18 +0530

    Add get_users function and corresponding tests
    Added a new function, get_users, to fetch user data from an external API using the `requests` library. Also, introduced unit tests to mock the API responses and verify the functionality of the get_users function, ensuring proper integration and reliability.
    
    Signed-off-by: Sharabh Shukla <sharabh2019@gmail.com>

--- tests/test_service.py
+++ tests/test_service.py
@@ -1,5 +1,5 @@
 import pytest
-from src.service import get_user_from_db
+from src.service import get_user_from_db, get_users
 import unittest.mock as mock
 
 @mock.patch("src.service.get_user_from_db")
@@ -8,6 +8,19 @@ def test_get_user_from_db(mock_get_user_from_db):
     user_name = mock_get_user_from_db(1)
 
     assert user_name == "Mocked Alice"
+
+
+@mock.patch("requests.get")
+def test_get_users_from_api(mock_get_users):
+    mock_response = mock.Mock()
+    mock_response.status_code = 200
+    mock_response.json.return_value = {'id': 1, "name": "JagatGuru"}
+    mock_get_users.return_value = mock_response
+    data = get_users()
+    assert data == {'id': 1, "name": "JagatGuru"}
+
+
+
 
 
 #
